94f960b468584cee5b67bc4bc42ea2445cc9dee8
Michael Meadows <mdmeadow>
2012-06-14 22:47:59 -0400

    Initial empty rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csc517Backchannel::Application.config.secret_token = '1f3ee86747393e58c0ae3b444277dce16922ba2e515b706cd2ff93eedaeab1b8a2b6e5d11a7d6b1a2c90cf6c563108d6457c356f127af4b6ad5d9da8859efd43'
