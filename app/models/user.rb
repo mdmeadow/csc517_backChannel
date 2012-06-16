5dd925b2f88e15a1f6fc0a9164cdd7fbf1751097
Michael Meadows <mdmeadow>
2012-06-16 00:57:33 -0400

    Added validates stmt to each model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   set_primary_key :user_id
   attr_accessible :isAdmin, :password, :userName, :user_id
+  validates :userName, :presence => true, :uniqueness => true
 end
