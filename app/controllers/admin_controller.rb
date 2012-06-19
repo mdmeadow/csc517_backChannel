9f1942aa99f15d3496f846c965a0ef02c9bf63ee
Michael Meadows <mdmeadow>
2012-06-19 00:32:41 -0400

    This checkin contains all my code to date.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,8 +5,13 @@ class AdminController < ApplicationController
     @postsPerUser = Post.select("userName, count(*) as post_count")
       .joins(:user).group("userName").order("post_count DESC")
 
-    @repliesPerPost = Post.select("posts.id, posts.body, posts.parent_id, userName, count(*) as reply_count")
-      .joins(:children).joins(:user).group("children_posts.parent_id").order("reply_count DESC")
+    @repliesPerPost = Post.select("posts.id, posts.body, posts.parent_id, userName, count(children_posts.parent_id) as reply_count")
+      .joins("left join posts as children_posts on posts.id = children_posts.parent_id").joins(:user).group("children_posts.parent_id").order("reply_count DESC")
+
+    @repliesPerPostForToday = Post.select("posts.id, posts.body, posts.parent_id, userName, count(children_posts.parent_id) as reply_count")
+      .where(["DATE(posts.created_at) >= ?", Date.today])
+      .joins("left join posts as children_posts on posts.id = children_posts.parent_id")
+      .joins(:user).group("children_posts.parent_id").order("reply_count DESC")
 
     @postsPerDay = Post.select("DATE(created_at) as date, count(*) as post_count")
       .group("date").order("post_count DESC")
