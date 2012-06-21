fabb43440559ec294eb9eef01a4e07e470463f76
Michael Meadows <mdmeadow>
2012-06-21 02:46:24 -0400

    bundle updating sqlite3

--- Gemfile
+++ Gemfile
@@ -5,9 +5,11 @@ gem 'rails', '3.2.5'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
-
-
+gem 'sqlite3', :group => [:development, :test]
+group :production do
+  gem 'thin'
+  gem 'pg'
+end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,9 @@ class HomeController < ApplicationController
       # @posts = Post.select("posts.id, posts.body, posts.parent_id, posts.user_id, posts.created_at, count(children_posts.parent_id) as reply_count")
       # .joins("left join posts as children_posts on children_posts.parent_id = posts.id").group("children_posts.parent_id").order("reply_count DESC")
       @posts = Post.where("parent_id is null")
+      # @posts = Post.find_by_sql("select posts.id, posts.body, posts.parent_id, posts.user_id, posts.created_at, count(children_posts.parent_id) as reply_count from posts left outer join" +
+        # " posts as children_posts on children_posts.parent_id = posts.id having group by children_posts.parent_id order by reply_count desc");
+
     else
       @backToIndex = true
     end
