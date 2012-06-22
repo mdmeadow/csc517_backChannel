bf026f20c831eb8891cff1b3f369b5df940e95ec
Michael Meadows <mdmeadow>
2012-06-22 01:53:12 -0400

    Issue with search and admin reports due to bug documented here: http://stackoverflow.com/questions/9765965/activerecord-column-does-not-exist. Only PostGres cares, sqlite worked properly.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,13 +2,13 @@ class AdminController < ApplicationController
   # GET /admin
   # GET /admin.json
   def index
-    @postsPerUser = Post.select("users.userName, count(*) as post_count")
+    @postsPerUser = Post.select('"userName", count(*) as post_count')
       .joins(:user).group("users.userName").order("post_count DESC")
 
     @repliesPerPost = Post.select('posts.id, posts.body, posts.parent_id, "userName"')
           .joins(:user).where("parent_id is null").sort_by{ |t| t.children.length }.reverse
 
-    @repliesPerPostForToday = Post.select("posts.id, posts.body, posts.parent_id, users.userName")
+    @repliesPerPostForToday = Post.select('posts.id, posts.body, posts.parent_id, "userName"')
           .joins(:user).where(["parent_id is null and DATE(posts.created_at) >= ?", Date.today]).sort_by{ |t| t.children.length }.reverse
 
     @postsPerDay = Post.select("DATE(created_at) as date, count(*) as post_count")
